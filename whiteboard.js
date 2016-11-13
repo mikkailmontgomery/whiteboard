75e4ad9479ea2ea532d4945f36b06312d34553bb
BuildTools <mikkail.montgomery@gmail.org>
2016-11-12 22:58:02 -0600

    Added Express, simplified,  touch support.
    Added Express, added touch, corrected socketio so it can be used beyond
    localhost.

--- whiteboard.js
+++ whiteboard.js
@@ -1,43 +1,17 @@
-var http = require('http')
-var url = require('url')
-var fs = require('fs')
-var path = require('path')
-var baseDirectory = __dirname   // or whatever base directory you want
+var app = require('express')();
+var http = require('http').Server(app);
+var uuid = require('hat')
+var io = require('socket.io')(http)
 
 var port = 8000
 var clickX = new Array();
 var clickY = new Array();
 var clickDrag = new Array();
 
-http.createServer(function (request, response) {
-   try {
-     var requestUrl = url.parse(request.url)
-		 if(requestUrl.pathname == "/"){
-			 requestUrl.pathname = "/whiteboard.html"
-		 }
-     // need to use path.normalize so people can't access directories underneath baseDirectory
-     var fsPath = baseDirectory+path.normalize(requestUrl.pathname)
-		 console.log(fsPath) //looking for a way to make a default document
-     response.writeHead(200)
-     var fileStream = fs.createReadStream(fsPath)
-     fileStream.pipe(response)
-     fileStream.on('error',function(e) {
-         response.writeHead(404)     // assume the file doesn't exist
-         response.end()
-     })
-   } catch(e) {
-     response.writeHead(500)
-     response.end()     // end the response so browsers don't hang
-     console.log(e.stack)
-   }
-}).listen(port)
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/whiteboard.html');
+});
 
-console.log("listening on port "+port)
-
-var uuid = require('hat')
-var iohttp = require('http')
-var iohttpserver = iohttp.createServer(function(request,response){})
-var io = require('socket.io')(iohttpserver)
 var clients = []
 io.on('connection', function(socket){
   var id = uuid()
@@ -54,8 +28,6 @@ io.on('connection', function(socket){
   });
 });
 
-
-iohttpserver.listen(8080)
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+http.listen(port, function(){
+  console.log('listening on *:'+port);
+});
